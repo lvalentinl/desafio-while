021f242593c3b2d74e1b2b910964912d68fa3250
J Valentin <jerber.valentin@gmail.com>
2025-01-21 14:55:54 -0500

    ejercicios terminados

--- app.js
+++ app.js
@@ -11,3 +11,32 @@ hasta ese número utilizando un bucle 'while' en la consola del navegador.
 Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 
 hasta ese número utilizando un bucle 'while' en la consola del navegador.
 */
+let contadorHasta10 = 1;
+let contadorHastaCero = 10;
+let contadorProgInicial = 0;
+let contadorRegresivo;
+let contadorProgresivo;
+
+while (contadorHasta10 <= 10) {
+    //console.log(contadorHasta10);
+    alert(`el valor es: ${contadorHasta10}`);
+    contadorHasta10++;
+}
+
+while (contadorHastaCero >= 0) {
+    //console.log(contadorHastaCero);
+    alert(`el valor es: ${contadorHastaCero}`);
+    contadorHastaCero--;
+}
+
+contadorRegresivo = prompt("Ingrese un número positivo:");
+while (contadorRegresivo >= 0 ) {
+    console.log(contadorRegresivo);
+    contadorRegresivo--;
+}
+
+contadorProgresivo = prompt("Ingrese un número positivo:");
+while (contadorProgInicial <= contadorProgresivo) {
+    console.log(contadorProgInicial);
+    contadorProgInicial++;
+}
